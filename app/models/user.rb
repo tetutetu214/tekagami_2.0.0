ca72bfd5261cb3d5948038ec2c82d67e3ed51581
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-24 11:39:32 +0900

    エラー文章　日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
