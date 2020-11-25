f256a72f4e15c11d87e606d89cc63ee493c172a5
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-25 19:22:58 +0900

    prep C 設定

--- app/controllers/preps_controller.rb
+++ app/controllers/preps_controller.rb
@@ -1,6 +1,23 @@
 class PrepsController < ApplicationController
 
   def index
+    @preps = Prep.all
+    @prep = Prep.new
   end
-  
+
+  def create
+    @prep = Prep.create(prep_params)
+    if @prep.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def prep_params
+    params.require(:prep).permit(:now, :target).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
    root 'tops#index'
-    resources :preps , only: [:index, :new, :create, :edit, :update, :destroy]
+    resources :preps , only: [:index, :create, :edit, :update, :destroy]
 end
