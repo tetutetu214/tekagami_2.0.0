c503090a81ae04e9b52874bba1874c2675638f97
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-25 12:46:31 +0900

    モデル　アソシエーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :preps
+  
   validates :nickname, presence: true
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
   validates :password, format: { with: VALID_PASSWORD_REGEX }
+
 end
