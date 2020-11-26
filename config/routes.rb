d5cde251f43eba1d250232d12e2d915cda3b6029
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-26 11:20:22 +0900

    task ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
     root 'tops#index'
-      resources :preps , only: [:index, :create, :edit, :update, :destroy] do
+      resources :preps , only: [:index, :create, :edit, :update, :destroy] 
         resources :tasks , only: [:index, :create, :edit, :update, :destroy]
-      end
+      
 end
