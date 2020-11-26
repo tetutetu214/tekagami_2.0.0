b7698c4343aaf325765a3af1892f4608b10764eb
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-26 09:43:28 +0900

    task アソシエーション・バリデーション

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
 class Task < ApplicationRecord
+  belongs_to :user
+  belongs_to :prep
+
+  with_options presence: true do
+    validates :task_1
+    validates :task_2
+    validates :task_3
+  end
+  
 end
