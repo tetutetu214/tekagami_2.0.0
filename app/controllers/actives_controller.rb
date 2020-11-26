89112a9d784ca4be1265b53ce85c08ed44ea0753
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-26 17:12:05 +0900

    active コントローラー・バリデーション・アソシエーション

---
+++ app/controllers/actives_controller.rb
@@ -0,0 +1,4 @@
+class ActivesController < ApplicationController
+  def index
+  end
+end
--- app/models/active.rb
+++ app/models/active.rb
@@ -1,2 +1,12 @@
 class Active < ApplicationRecord
+  belongs_to :user
+  belongs_to :prep
+  belongs_to :tasks
+
+  with_options presence: true do
+    validates :way_1
+    validates :way_2
+    validates :way_3
+  end
+
 end
