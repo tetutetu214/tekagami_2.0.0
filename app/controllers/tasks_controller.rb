b41ef32b693dabc8fcbcd230a903c559a465e723
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-26 11:07:40 +0900

    task ルーティングネスト変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,21 @@
 class TasksController < ApplicationController
   def index
   end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.valid?
+      @task.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_1, :task_2,:task_3).merge(user_id: current_user.id ,prep_id: params[:prep_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
     root 'tops#index'
-      resources :preps , only: [:index, :create, :edit, :update, :destroy]
+      resources :preps , only: [:index, :create, :edit, :update, :destroy] do
         resources :tasks , only: [:index, :create, :edit, :update, :destroy]
-     
+      end
 end
