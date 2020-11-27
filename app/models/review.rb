e3f2ac71fdeeea4dea3a38bad3a5d8329a384786
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-27 11:56:16 +0900

    review バリデーション

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,11 @@ class Review < ApplicationRecord
   belongs_to :prep
   belongs_to :task
   belongs_to :active
+
+  with_options presence: true do
+    validates :rev_1
+    validates :rev_2
+    validates :rev_3
+  end
+
 end
