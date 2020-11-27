956be19e23ca547e740b735f9b45ccf3bf0a7dc0
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-27 10:36:31 +0900

    reviews DB

---
+++ db/migrate/20201127005259_create_reviews.rb
@@ -0,0 +1,14 @@
+class CreateReviews < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reviews do |t|
+      t.text :rev_1,  null: false
+      t.text :rev_2,  null: false
+      t.text :rev_3,  null: false
+      t.references :user ,foreign_key: true 
+      t.references :prep ,foreign_key: true 
+      t.references :task ,foreign_key: true 
+      t.references :active ,foreign_key: true 
+      t.timestamps
+    end
+  end
+end
