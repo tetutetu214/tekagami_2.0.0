5bdfadad10853fbb76b56a3655335739c897ce02
tetutetu214 <lemoned.i.scream.art.of.noise@gmail.com>
2020-11-28 12:18:17 +0900

    Update environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,4 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
 
 const webpack = require('webpack')
 environment.plugins.prepend(
